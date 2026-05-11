1229d492980cd02e46587c62024aaa6e7f7dc2e9
serenananana6 <1418393@learn.vsb.bc.ca>
2026-05-11 20:35:35 +0000

    g

--- myApp.js
+++ myApp.js
@@ -2,8 +2,19 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
+require('dotenv').config()
 
-
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function(req, res) {
+//res.sendFile (__dirname + "/views/index.html");
+//res.json({
+  if (process.env.MESSAGE_STYLE==="uppercase"){
+    res.json({"message": "HELLO JSON"});
+  }else{
+    res.json({"message": "Hello json"});
+  }
+  });
+//});
 
 
 
